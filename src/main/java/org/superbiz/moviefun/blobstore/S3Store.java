0002da351042699a0d47a5f0d84d0e8dd4ba64cd
Nikhil and Dai <ltrieu2@ford.com>
2019-04-29 16:04:46 +0100

    Add Exception Handling because of the Interface

--- src/main/java/org/superbiz/moviefun/blobstore/S3Store.java
+++ src/main/java/org/superbiz/moviefun/blobstore/S3Store.java
@@ -5,9 +5,10 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
 
+import java.io.IOException;
 import java.util.Optional;
 
-public class S3Store implements BlobStore {
+public class S3Store implements BlobStore  {
 
     private AmazonS3Client s3Client;
     private String photoStorageBucket;
@@ -18,14 +19,14 @@ public class S3Store implements BlobStore {
     }
 
     @Override
-    public void put(Blob blob) {
+    public void put(Blob blob) throws IOException {
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentType(blob.contentType);
         s3Client.putObject(photoStorageBucket, blob.name, blob.inputStream, metadata);
     }
 
     @Override
-    public Optional<Blob> get(String name) {
+    public Optional<Blob> get(String name) throws IOException{
         if(s3Client.doesObjectExist(photoStorageBucket, name)) {
             S3Object s3Object = s3Client.getObject(photoStorageBucket, name);
             Blob blob = new Blob(s3Object.getKey(), s3Object.getObjectContent(), s3Object.getObjectMetadata().getContentType());
